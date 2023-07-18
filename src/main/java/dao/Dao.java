6a64de506135a4cff08bc49cb456db143f3f0158
Oksana Poliakova <it.oksana.poliakova@gmail.com>
2023-07-18 12:49:04 +0300

    Added the FlightStatus entity. Added Dao.

---
+++ src/main/java/dao/Dao.java
@@ -0,0 +1,20 @@
+package dao;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author Oksana Poliakova on 18.07.2023
+ * @projectName FlightMVC
+ */
+public interface Dao <K, T> {
+    List<T> findAll();
+
+    Optional<T> findById(K id);
+
+    boolean delete(K id);
+
+    void update(T entity);
+
+    T save(T entity);
+}
---
+++ src/main/java/entity/FlightStatus.java
@@ -0,0 +1,12 @@
+package entity;
+
+/**
+ * @author Oksana Poliakova on 18.07.2023
+ * @projectName FlightMVC
+ */
+public enum FlightStatus {
+    ARRIVED,
+    DEPARTED,
+    CANCELLED,
+    SCHEDULED
+}
