a97d6edfe2c97657e88821c202dba087ca4e7acd
Oksana Poliakova <it.oksana.poliakova@gmail.com>
2023-07-18 17:18:51 +0300

    Added the FlightServlet

---
+++ src/main/java/servlet/FlightServlet.java
@@ -0,0 +1,40 @@
+package servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import service.FlightService;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author Oksana Poliakova on 18.07.2023
+ * @projectName FlightMVC
+ */
+@WebServlet("/flights")
+public class FlightServlet extends HttpServlet {
+
+    private final FlightService flightService = FlightService.getInstance();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
+
+        try (var printWriter = resp.getWriter()) {
+            printWriter.write("<h1>Список перелетов:</h1>");
+            printWriter.write("<ul>");
+            flightService.findAll().forEach(flightDto -> {
+                printWriter.write("""
+                        <li>
+                            <a href="/tickets?flightId=%d">%s</a>
+                        </li>
+                        """.formatted(flightDto.getId(), flightDto.getDescription()));
+            });
+            printWriter.write("</ul>");
+        }
+    }
+}
