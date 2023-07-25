4086de68507b0fceca258d430ecafd1ee2880009
Oksana Poliakova <it.oksana.poliakova@gmail.com>
2023-07-25 19:54:14 +0300

    Added validations for other cases

--- src/main/java/dto/validator/CreateUserValidator.java
+++ src/main/java/dto/validator/CreateUserValidator.java
@@ -19,14 +19,21 @@ public class CreateUserValidator implements Validator<CreateUserDto> {
         if (Gender.find(object.getGender()).isEmpty()) {
             validationResult.add(Error.of("invalid.gender", "Gender is invalid"));
         }
+        if (object.getName().isEmpty()) {
+            validationResult.add(Error.of("invalid.name", "Name is invalid"));
+        }
+        if (object.getPassword().isEmpty()) {
+            validationResult.add(Error.of("invalid.password", "Password is invalid"));
+        }
+        if (object.getEmail().isEmpty()) {
+            validationResult.add(Error.of("invalid.email", "Email is invalid"));
+        }
         if (Role.find(object.getRole()).isEmpty()) {
             validationResult.add(Error.of("invalid.role", "Role is invalid"));
         }
         if (!LocalDateFormatter.isValid(object.getBirthday())) {
             validationResult.add(Error.of("invalid.birthday", "Birthday is invalid"));
         }
-        // TODO: Write validations for other cases
-
         return validationResult;
     }
 
